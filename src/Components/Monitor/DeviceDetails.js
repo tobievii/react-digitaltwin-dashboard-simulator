0e57ef8713a378f4f8601e06c620e6bebf144ab6
Sourav Debnath <sourav2012d@gmail.com>
2018-11-12 13:47:12 +0530

    Update DeviceDetails.js
    Minor Fix

--- src/Components/Monitor/DeviceDetails.js
+++ src/Components/Monitor/DeviceDetails.js
@@ -5,7 +5,7 @@ const deviceDetails = props => {
   return (
     <div className="device-info-header">
       <hr className="horizontal-line-below-status" />
-      <span className="device-name">Air Handler Unit - Building ITC </span>
+      <span className="device-name">Air Handler Unit</span>
       <br /> <br />
       <span className="device-id">9C7HSWT-882UQF0C-SOURAV11-WVJCJTK47</span>
     </div>
